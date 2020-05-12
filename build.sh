2c920d575578a0aac70042d1aeb748423264f18c
Uri Herrera <hello@uriherrera.com>
2020-05-11 21:50:35 -0500

    update
    - this code doesn't work anymore but I updated it in order to have something that compiles

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt -yy -qq update
+apt -yy -qq install wget gnupg2
+
+### Add KDENeon Repository
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 55751E5D 1B69B2DA > /dev/null
+wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/nitrux-iso-tool/development/configs/files/sources.list.neon.user
+wget -qO /etc/apt/sources.list.d/nitrux-repo-.list https://raw.githubusercontent.com/Nitrux/nitrux-iso-tool/development/configs/files/sources.list.nitrux
+apt -yy -qq update
+
+### Install Dependencies
+apt -yy -qq install cmake extra-cmake-modules qtbase5-dev git qtdeclarative5-dev libqt5xmlpatterns5-dev autotools-dev automake libtool libgtk2.0-dev libfuse-dev libappimage-dev patchelf  xxd desktop-file-utils 
+
+## Build
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_PREFIX=Release -DCMAKE_INSTALL_PREFIX=/usr
+make
--- cmake/libappimage.cmake
+++ cmake/libappimage.cmake
@@ -1,4 +1,4 @@
-message(STATUS "Downloading and building LibAppImage")
+message(STATUS "Downloading and building libappimage")
 
 find_package(AppImageKit QUIET)
 
@@ -7,7 +7,7 @@ if ( NOT AppImageKit_FOUND)
 
     ExternalProject_Add(AppImageKit
         GIT_REPOSITORY https://github.com/AppImage/AppImageKit.git
-        GIT_TAG appimagetool/master
+        GIT_TAG master
         GIT_SUBMODULES ""
         CONFIGURE_COMMAND ${CMAKE_COMMAND} -G${CMAKE_GENERATOR} -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} <SOURCE_DIR>
         BUILD_COMMAND make gtest libappimage
@@ -17,7 +17,7 @@ if ( NOT AppImageKit_FOUND)
     ExternalProject_Get_Property(AppImageKit INSTALL_DIR)
     set(AppImageKit_INSTALL_DIR ${INSTALL_DIR})
 
-    set(libappimage_PATH ${AppImageKit_INSTALL_DIR}${CMAKE_INSTALL_PREFIX}/lib/libappimage.so)
+    set(libappimage_PATH ${AppImageKit_INSTALL_DIR}${CMAKE_INSTALL_PREFIX}/lib/x86_64-linux-gnu/libappimage.so)
     set(libappimage_INCLUDE_DIRECTORIES ${AppImageKit_INSTALL_DIR}${CMAKE_INSTALL_PREFIX}/include/)
 
 
